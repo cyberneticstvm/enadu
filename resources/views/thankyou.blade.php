b73269b2125d9e02e03c7b86a32e16207b4b9756
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-01-27 18:41:12 +0530

    small updates

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,13 +1,13 @@
 @extends('base')
 @section('content')
-<div class="p-3 shadow-sm bg-warning danger-nav osahan-home-header">
+<div class="p-3 shadow-sm bg-success danger-nav osahan-home-header">
 	<div class="font-weight-normal mb-0 d-flex align-items-center">
-		<h6 class="fw-normal mb-0 text-dark d-flex align-items-center">
-			<a class="text-dark me-3 fs-4" href="/geebinapp"><i class="bi bi-chevron-left"></i></a>
+		<h6 class="fw-normal mb-0 text-white d-flex align-items-center">
+			<a class="text-white me-3 fs-4" href="/geebinapp"><i class="bi bi-chevron-left"></i></a>
 			Payment Status
 		</h6>
 		<div class="ms-auto d-flex align-items-center">
-			<a class="toggle osahan-toggle fs-4 text-dark ms-auto" href="#"><i class="bi bi-list"></i></a>
+			<a class="toggle osahan-toggle fs-4 text-white ms-auto" href="#"><i class="bi bi-list"></i></a>
 		</div>
 	</div>
 </div>

32c4a124fd3b392639c44ff13bc541896ac6204d
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-14 08:56:22 +0530

    small updates

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -74,7 +74,7 @@ class CartController extends Controller
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
             //curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-Api-Key:$key", "X-Auth-Token:$token"));
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(array("Authorization: Bearer $token")));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer $token"));
             $payload = Array(
                 'purpose' => $request->purpose,
                 'amount' => $request->amount,
